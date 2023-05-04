8ab79a13fca3f7a8aa569eb90b7d706c8eff37a8
202344076 <128006933+PARINSER@users.noreply.github.com>
2023-05-04 12:09:59 +0900

    Add files via upload

---
+++ PracticeEx/PracticeEx-Lab-1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int factorial(int n){
+    if(n==0)
+        return 1;
+    else
+        return n*factorial(n-1);
+}
+
+int main(){
+   int n;
+
+   printf("수를 입력하세요");
+   scanf("%d",&n);
+
+   printf("%d",factorial(n));
+
+   return 0; 
+}
